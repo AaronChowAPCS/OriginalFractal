3be0c0fc13b6eeb77ae6c894e78c79f045476db9
aaronchowapcs <aahchow@gmail.com>
2015-02-10 11:03:49 -0800

    Final Fractal Update.

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,20 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
+float changeValue;
 public void setup()
 {
 	background(0);
@@ -8,7 +25,7 @@ public void setup()
 }
 public void draw()
 {
-	
+
 }
 public void fractal(int x, int y, int len)
 {
@@ -28,4 +45,13 @@ public void fractal(int x, int y, int len)
 		//fractal(x,y-len/2,len/2);
 		//fractal(x,y+len/2,len/2);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
